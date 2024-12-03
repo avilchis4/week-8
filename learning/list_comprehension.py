a1c8e382723f11dc0471015ac5c80c1d84546deb
Vilchis <avilchis4@cps.k12.il.us>
2024-12-03 09:16:36 -0600

    list_comprehension

--- learning/list_comprehension.py
+++ learning/list_comprehension.py
@@ -1,4 +1,44 @@
 
+#list comprehension: a concise way to create lists in python
+                    #compact and easier to read than tradtional loops
+                    #[expression for value in iterable if condtion]
+
+#for loop when you have start and end; while loop when we don't know end
+
+# doubles = []
+
+# for x in range (1, 11):
+#     doubles.append(x * 2) #dump things in empty list and everytime times 2
+
+# print (doubles)
+
+
+# doubles = [x * 2 for x in range (1, 11)]
+# triples = [y * 3 for y in range (1,11)]
+# squares = [z * z for z in range (1,11)]
+# print(doubles)
+# print(triples)
+# print(squares)````````````````````````````````````````````
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+
+# # fruits_chars = [fruit[0] for fruit in fruits]
+# fruits_chars = [fruit[1].upper for fruit in fruits]
+# # fruits_chars = [fruit.upper() for fruit in fruits]
+# print(fruits)
+
+# numbers = [1,-2, 3,-4, 5, 6]
+# positive_nums = [num for num in numbers if num >= 0]
+# negative_nums = [num for num in numbers if num < 0]
+# even_nums = [num for num in numbers if num % 2 == 0]
+# print(positive_nums)
+# print(negative_nums)
+# print(even_nums)
+
+# grades = [85, 42, 79, 90, 56, 61, 30]
+# passing_grades= [grade for grade in grades if grade >= 60]
+
+# print(passing_grades)
 
 ################################################List comprehension###############################################
 # List Comprehensions Practice #1
